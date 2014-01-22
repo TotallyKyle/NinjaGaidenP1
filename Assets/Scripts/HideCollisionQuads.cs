265431a8e7d6872102e9f2fc361a196e0b4a429b
TotallyKyle <kylez@umich.edu>
2014-01-22 11:41:58 -0500

    Initial Run FSM and Camera Script

---
+++ Assets/Scripts/HideCollisionQuads.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideCollisionQuads : MonoBehaviour {
+	
+	void Awake() {
+		foreach (Transform t in transform) {
+			Destroy(t.gameObject);
+		}
+	}
+	
+}
---
+++ Assets/Scripts/RyuAnimatorController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RyuAnimatorController : MonoBehaviour
+{
+
+    private Animator animator;
+    const int RIGHT = 1;
+    const int LEFT = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+
+        if (horizontal > 0)
+            animator.SetInteger("Direction", RIGHT);
+        else if (horizontal < 0)
+            animator.SetInteger("Direction", LEFT);
+
+        if (horizontal == 0)
+            animator.SetInteger("Motion", 0);
+        else
+            animator.SetInteger("Motion", 1);
+    }
+}
