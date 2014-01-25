a5afae08a9def832d37464c80d5b096317e0193b
Anthony Kause <akause@barracudanetworks.com>
2014-01-25 16:28:59 -0500

    Change camera follow to offset vertically on Ryu

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * Script that positions the main camera on a given point of 
+ * interest (Ryu) with the correct offset to show the whole height of the map.
+ */
 public class CameraFollow : MonoBehaviour {
-	public Transform 	poi; // Point of Interest
-	public float		u;
-	public Vector3		offset = new Vector3(0,0,-5);
 	
-	// Update is called once per frame
+	// Point of Interest (i.e. Ryu for the Main Camera)
+	public Transform 	poi;
+
+	public Vector3		offset = new Vector3(0,12.625f,-5);
+	
 	void Update () {
-		Vector3 poiV3 = poi.position + offset;
-		Vector3 currPos = transform.position;
-		Vector3 pos = (1-u)*currPos + u*poiV3;
-		transform.position = pos;
+		Vector3 offsetPos = poi.position + offset;
+		transform.position = offsetPos;
 	}
 }
