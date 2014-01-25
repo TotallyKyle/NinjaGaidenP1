141e408eeb6fb1921c7bf4c1b488206211ef7390
TotallyKyle <kylez@umich.edu>
2014-01-25 13:16:47 -0500

    Used tags and layers, got a wall implemented
    Need to figure out some way to determine which direction the wall is.

--- Assets/Scripts/Ryu.cs
+++ Assets/Scripts/Ryu.cs
@@ -1,40 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Ryu : MonoBehaviour {
-	public float		speed = 8;
-	public float		jumpSpeed = 12;
-	public float		jumpAcc = 4;
-	
-	public bool			grounded = true;
-	
-	void Update () { // Every Frame
-		float h = Input.GetAxis ("Horizontal");
-		float v = Input.GetAxis ("Vertical");
-		
-		Vector2 vel = rigidbody2D.velocity;
-		vel.x = h*speed;
-		
-		if (Input.GetKeyDown(KeyCode.Space) ||
-		    Input.GetKeyDown(KeyCode.UpArrow) ||
-		    Input.GetKeyDown(KeyCode.W) ) {
-			if (grounded) vel.y = jumpSpeed;
-		}
-		if (Input.GetKey(KeyCode.Space) ||
-		    Input.GetKey(KeyCode.UpArrow) ||
-		    Input.GetKey(KeyCode.W) ) {
-			if (!grounded) vel.y += jumpAcc * Time.deltaTime;
-		}
-		
-		rigidbody2D.velocity = vel;
-	}
-	
-	void OnTriggerEnter2D(Collider2D other) {
-		grounded = true;
-	}
-	void OnTriggerExit2D(Collider2D other) {
-		grounded = false;
-	}
+public class Ryu : MonoBehaviour
+{
+    //Variable mechanics like running and jumping speed
+    public float speed = 8;
+    public float jumpSpeed = 24;
+    public Vector2 playerVelocity;
+
+    //Input from User
+    public float horizontalInput;
+    public float verticalInput;
+
+    //Conditions that Ryu is experiencing
+    public bool climbing = false;
+    public bool grounded = true;
+
+    //Constants
+    const int playerLAYER = 8;
+    const int wallLAYER = 9;
+
+    void Update()
+    { // Every Frame
+        if (!climbing)
+            horizontalInput = Input.GetAxis("Horizontal");
+        else
+            horizontalInput = 0;
+        verticalInput = Input.GetAxis("Vertical");
+
+        playerVelocity = rigidbody2D.velocity;
+        playerVelocity = horizontalInput * speed;
+
+        //Ryu's Jumping Motion
+        if (Input.GetKeyDown(KeyCode.Space) ||
+            Input.GetKeyDown(KeyCode.UpArrow) ||
+            Input.GetKeyDown(KeyCode.W))
+        {
+            if (grounded || climbing)
+                playerVelocity.y = jumpSpeed;
+        }
+
+        rigidbody2D.velocity = playerVelocity;
+
+        //Ignoring wall collisions when Ryu is grounded
+        if (grounded)
+            Physics2D.IgnoreLayerCollision(playerLAYER, wallLAYER, true);
+        else
+            Physics2D.IgnoreLayerCollision(playerLAYER, wallLAYER, false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Ground")
+            grounded = true;
+        else if (other.tag == "Wall")
+            climbing = true;
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Ground")
+            grounded = false;
+        else if (other.tag == "Wall")
+            climbing = false;
+    }
 }
 
 
