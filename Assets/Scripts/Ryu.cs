1189b6fe05f55f664ddb45e8b18ff652a2326153
Anthony Kause <akause@barracudanetworks.com>
2014-01-27 15:12:09 -0500

    Reduce states without left/right

--- Assets/Scripts/Ryu.cs
+++ Assets/Scripts/Ryu.cs
@@ -6,19 +6,9 @@ public class Ryu : MonoBehaviour
     /*
      * Variable mechanics like running and jumping speed
      */
-	// 1.5 pixels/frame / 16 pixels/meter * 60 frames/second
-    public float runSpeed = 5.625f; 
-	// Using a gravity of -1m/s*s, the velocity was found using:
-	// t = 21 frames / 60 fps
-	// h = 48px / 16px / meter
-	// v = (h + t^2) / t = 8.92142857
+    public float mSpeed = 1.5f / 16f * 60f; 
+	public float mSlowSpeed = 0.5f / 16f * 60f;
     public float jumpSpeed = 8.92142857f;
-    public Vector2 playerVelocity;
-    public Vector3 playerPosition;
-
-    //Input from User
-    public float horizontalInput;
-    public float verticalInput;
 
     //Conditions that Ryu is experiencing
     public bool climbing = false;
@@ -28,62 +18,70 @@ public class Ryu : MonoBehaviour
     const int playerLAYER = 8;
     const int wallLAYER = 9;
 
-    void Update()
-    { // Every Frame
+	private Animator mAnimator;
 
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-        playerPosition = transform.position;
-        playerVelocity = rigidbody2D.velocity;
+	/*
+	 * true if the player is on some ground, false otherwise
+	 */
+	public bool mGrounded = true;
+	public Transform mGroundCheck;
+	public float mGroundRadius = 0.2f;
 
-        /***Ryu's Horizontal Motion***/
-        //Running
-        if (horizontalInput > 0)
-            playerVelocity.x = runSpeed;
-        else if (horizontalInput < 0)
-            playerVelocity.x = -runSpeed;
-        else
-            playerVelocity.x = 0;
+	/*
+	 * Tells the collider what to consider ground
+	 */
+	public LayerMask mGroundLayer;
 
-        if (climbing)
-            playerVelocity.x = 0;
+	public bool mFacingRight = true;
 
-        /***Ryu's Vertical Motion***/
-        //Jumping
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            if (grounded)
-                playerVelocity.y = jumpSpeed;
-        }
+	void Start() {
+		mAnimator = GetComponent<Animator>();
+	}
 
-        //Climbing
-        if (climbing)
-            playerVelocity.y = 0;
+	void Update() {
+		bool jumpKey = Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.RightAlt);
+		if (mGrounded && jumpKey) {
+			mAnimator.SetBool("Grounded", false);
+			rigidbody2D.AddForce(new Vector2(0, 1000f));
+		}
+	}
 
-        //Jumping off Wall
-        AnimatorStateInfo state = GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
-        if (state.IsName("Hang Right"))
-        {
-            if (Input.GetKey(KeyCode.Z) && horizontalInput < 0)
-            {
-                rigidbody2D.WakeUp();
-                climbing = false;
-                playerVelocity.y = jumpSpeed;
-                playerVelocity.x = -runSpeed;
-            }
-        }
-        else if (state.IsName("Hang Left"))
-        {
-            if (Input.GetKey(KeyCode.Z) && horizontalInput > 0)
-            {
-                rigidbody2D.WakeUp();
-                climbing = false;
-                playerVelocity.y = jumpSpeed;
-                playerVelocity.x = runSpeed;
-            }
-        }
+    void FixedUpdate() {
+		/*
+		 * Check if we are on the ground or not by casting downward 
+		 */
+		mGrounded = Physics2D.OverlapCircle(mGroundCheck.position, mGroundRadius, mGroundLayer);
+		mAnimator.SetBool("Grounded", mGrounded);
 
-        rigidbody2D.velocity = playerVelocity;
+		/*
+		 * Handle horizontal movement
+		 */
+		float velocity = 0f;
+		if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow)) {
+			if (!mGrounded) {
+				if (mFacingRight)
+					velocity = -1 * mSlowSpeed;
+				else
+					velocity = -1 * mSpeed;
+			} else {
+				velocity = -1 * mSpeed;
+				if (mFacingRight)
+					flip();
+			}
+		} else if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow)) {
+			if (!mGrounded) {
+				if (!mFacingRight)
+					velocity = mSlowSpeed;
+				else
+					velocity = mSpeed;
+			} else {
+				velocity = mSpeed;
+				if (!mFacingRight)
+					flip();
+			}
+		}
+		mAnimator.SetFloat("Speed", Mathf.Abs(velocity));
+		rigidbody2D.velocity = new Vector2(velocity, rigidbody2D.velocity.y);
 
         //Ignoring wall collisions when Ryu is grounded
         if (grounded)
@@ -91,6 +89,13 @@ public class Ryu : MonoBehaviour
         else
             Physics2D.IgnoreLayerCollision(playerLAYER, wallLAYER, false);
     }
+
+	private void flip() {
+		mFacingRight = !mFacingRight;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
 
     void OnTriggerEnter2D(Collider2D other)
     {
