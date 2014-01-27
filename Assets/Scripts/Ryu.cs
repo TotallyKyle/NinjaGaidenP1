1dafe20b69823968364e60256a8777507a3a327d
Anthony Kause <akause@barracudanetworks.com>
2014-01-27 16:21:47 -0500

    Started other way of animation

--- Assets/Scripts/Ryu.cs
+++ Assets/Scripts/Ryu.cs
@@ -1,48 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Ryu : MonoBehaviour
-{
+[System.Serializable]
+public class RyuAnimation {
+	public string name;
+	public Sprite[] frames;
+}
+
+public class Ryu : MonoBehaviour {
+
+	// Constants
+	// =============================================
+
+	/*
+	 * Layer indecies 
+	 */
+	private const int LAYER_PLAYER	= 8;
+	private const int LAYER_WALLS	= 9;
+
     /*
      * Variable mechanics like running and jumping speed
      */
-    public float mSpeed = 1.5f / 16f * 60f; 
-	public float mSlowSpeed = 0.5f / 16f * 60f;
-    public float jumpSpeed = 8.92142857f;
+    public const float SPEED		= 1.5f / 16f * 50f; 
+	public const float SPEED_MED	= 1.0f / 16f * 50f;
+	public const float SPEED_SLOW	= 0.5f / 16f * 50f;
 
-    //Conditions that Ryu is experiencing
-    public bool climbing = false;
-    public bool grounded = true;
-
-    //Constants
-    const int playerLAYER = 8;
-    const int wallLAYER = 9;
-
-	private Animator mAnimator;
+	// Ground raycasting
+	// ============================================
 
 	/*
-	 * true if the player is on some ground, false otherwise
+	 * true if Ryu is on some ground, false otherwise
 	 */
-	public bool mGrounded = true;
-	public Transform mGroundCheck;
-	public float mGroundRadius = 0.2f;
+	public bool grounded = true;
+	public Transform groundCheck;
+	public float groundRadius = 0.2f;
 
 	/*
 	 * Tells the collider what to consider ground
 	 */
-	public LayerMask mGroundLayer;
+	public LayerMask groundLayer;
 
-	public bool mFacingRight = true;
+	// Wall raycasting
+	// ============================================
+
+	/*
+	 * true if Ryu is on a wall, false otherwise
+	 */
+	public bool climbing = false;
+	public Transform wallCheck;
+	public float wallRadius = 0.2f;
+
+	/*
+	 * Tells the collider what to consider walls
+	 */
+	public LayerMask wallLayer;
+
+	// State
+	// =====================================
+
+	public bool facingRight = true;
+
+	/*
+	 * Array of all of Ryu's different state animations
+	 */
+	public RyuAnimation[] ryuAnimations;
 
 	void Start() {
-		mAnimator = GetComponent<Animator>();
 	}
 
 	void Update() {
 		bool jumpKey = Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.RightAlt);
-		if (mGrounded && jumpKey) {
-			mAnimator.SetBool("Grounded", false);
-			rigidbody2D.AddForce(new Vector2(0, 1000f));
+		if (grounded && jumpKey) {
+			// Animate jump
+			rigidbody2D.AddForce(new Vector2(0, 1500f));
 		}
 	}
 
@@ -50,68 +80,60 @@ public class Ryu : MonoBehaviour
 		/*
 		 * Check if we are on the ground or not by casting downward 
 		 */
-		mGrounded = Physics2D.OverlapCircle(mGroundCheck.position, mGroundRadius, mGroundLayer);
-		mAnimator.SetBool("Grounded", mGrounded);
+		grounded = 
+			Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+
+		// If grounded, ignore collisions with walls
+		Physics2D.IgnoreLayerCollision(LAYER_PLAYER, LAYER_WALLS, grounded);
 
 		/*
-		 * Handle horizontal movement
-		 */
-		float velocity = 0f;
-		if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow)) {
-			if (!mGrounded) {
-				if (mFacingRight)
-					velocity = -1 * mSlowSpeed;
-				else
-					velocity = -1 * mSpeed;
-			} else {
-				velocity = -1 * mSpeed;
-				if (mFacingRight)
-					flip();
-			}
-		} else if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow)) {
-			if (!mGrounded) {
-				if (!mFacingRight)
-					velocity = mSlowSpeed;
-				else
-					velocity = mSpeed;
-			} else {
-				velocity = mSpeed;
-				if (!mFacingRight)
-					flip();
-			}
-		}
-		mAnimator.SetFloat("Speed", Mathf.Abs(velocity));
-		rigidbody2D.velocity = new Vector2(velocity, rigidbody2D.velocity.y);
+		 * Check if we are climbing on a wall by casting to the side
+		 */ 
+		climbing = !grounded && 
+			Physics2D.OverlapCircle(wallCheck.position, wallRadius, wallLayer);
+		if (climbing) {
+			rigidbody2D.Sleep();
+			// Animate climb
+		} else {
 
-        //Ignoring wall collisions when Ryu is grounded
-        if (grounded)
-            Physics2D.IgnoreLayerCollision(playerLAYER, wallLAYER, true);
-        else
-            Physics2D.IgnoreLayerCollision(playerLAYER, wallLAYER, false);
+			/*
+		 	 * Handle horizontal movement
+		 	 */
+			float velocity = 0f;
+			if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow)) {
+				if (!grounded) {
+					if (facingRight)
+						velocity = -1 * SPEED_SLOW;
+					else
+						velocity = -1 * SPEED;
+				} else {
+					velocity = -1 * SPEED;
+					if (facingRight)
+						flip();
+				}
+			} else if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow)) {
+				if (!grounded) {
+					if (!facingRight)
+						velocity = SPEED_SLOW;
+					else
+						velocity = SPEED;
+				} else {
+					velocity = SPEED;
+					if (!facingRight)
+						flip();
+				}
+			}
+			// Animate idle or running
+			rigidbody2D.velocity = new Vector2(velocity, rigidbody2D.velocity.y);
+		}
     }
 
 	private void flip() {
-		mFacingRight = !mFacingRight;
+		facingRight = !facingRight;
 		Vector3 scale = transform.localScale;
 		scale.x *= -1;
 		transform.localScale = scale;
 	}
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Ground")
-            grounded = true;
-        else if (other.tag == "Wall")
-        {
-            climbing = true;
-            rigidbody2D.Sleep();
-        }
-    }
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.tag == "Ground")
-            grounded = false;
-    }
 }
 
 
