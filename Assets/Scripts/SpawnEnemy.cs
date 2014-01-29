082767190b0bba4353d6eb4b684e05b9ac2649e4
TotallyKyle <kylez@umich.edu>
2014-01-29 16:14:36 -0500

    Added first enemy spawnpoint and Batman animations & behavior
    Still need attack and stuff

---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    //Enemy Prefab
+    public Transform enemy;
+
+    void Update()
+    {
+        Transform enemySpawned;
+        GameObject camera = GameObject.Find("Main Camera");
+        if ((Mathf.Abs(camera.transform.position.x - transform.position.x) < .05f) && (transform.childCount < 1))
+        {
+            enemySpawned = Instantiate(enemy, new Vector3(transform.position.x + 7.4f, transform.position.y, 0), Quaternion.identity) as Transform;
+            enemySpawned.parent = transform;
+        }
+    }
+}
