788e88980c4d25acc2977400f5e6eebe03b9e6f9
TotallyKyle <kylez@umich.edu>
2014-02-03 22:49:06 -0500

    Finished Game HUD

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -27,65 +27,74 @@ public class GameData : MonoBehaviour {
     public Texture2D dash;
 
     //The Variables that actually hold the information
-    public int scoreData = 120;
-    public int timerData = 111;
-    public int healthData = 4;
+    public int scoreData = 0;
+    public int timerData = 0;
+    public int healthData = 0;
     public int spiritData = 0;
     public int livesData = 0;
 
     // Use this for initialization
     void Start() {
-        //Array of Textures for Hashing
-        Texture2D[] numberArray = { zero, one, two, three, four, five, six, seven, eight, nine };
-
-        //Score
-        createHUD("Score", .15f, 0, score, 75, -15, 10, 10);
-
-        //Score Digits
-        for (int i = 1; i < 7; i++) {
-            createHUD("Score " + i + " Digit", 0, 0, numberArray[(int)Mathf.Floor(scoreData / Mathf.Pow(10, 6 - i)) % 10], 118 + (i - 1) * 11, -15, 10, 10);
-        }
-
-        //Timer
-        createHUD("Timer", .15f, 0, timer, 75, -27, 10, 10);
-
-        //Timer Digits
-        for (int i = 1; i < 4; i++) {
-            createHUD("Timer " + i + " Digit", 0, 0, numberArray[(int)Mathf.Floor(timerData / Mathf.Pow(10, 3 - i)) % 10], 118 + (i - 1) * 11, -27, 10, 10);
-        }
-
-        //Lives
-        createHUD("Lives", .02f, 0, lives, 52, -39, 10, 10);
-
-        //Lives Digits
-        for (int i = 1; i < 3; i++) {
-            createHUD("Lives " + i + " Digit", 0, 0, numberArray[(int)Mathf.Floor(livesData / Mathf.Pow(10, 2 - i)) % 10], 70 + (i - 1) * 11, -39, 10, 10);
-        }
-
-        //Spiritual Power
-        createHUD("Spirit Power", .025f, 0, spiritPower, 99, -39, 10, 10);
-
-        //Spirit Power Digits
-        for (int i = 1; i < 4; i++) {
-            createHUD("Spirit Power " + i + " Digit", 0, 0, numberArray[(int)Mathf.Floor(spiritData / Mathf.Pow(10, 3 - i)) % 10], 119 + (i - 1) * 11, -39, 10, 10);
-        }
-
-        //Stage
-        createHUD("Stage", .15f, 0, stage, 250, -15, 10, 10);
-
-        //Power Up
-        createHUD("Power Up", .15f, 0.05f, powerUp, 181, -35, 10, 10);
-
-        //Ryu's Health
-        createHUD("Ninja", .15f, 0, ninja, 250, -27, 10, 10);
-
-        //Ryu's Health Bars
 
     }
 
     // Update is called once per frame
     void Update() {
+        Transform gameHUD = GameObject.Find("Game HUD").transform;
+        foreach (Transform child in gameHUD)
+            Destroy(child.gameObject);
 
+        //Array of Textures for Hashing
+        Texture2D[] numberArray = { zero, one, two, three, four, five, six, seven, eight, nine };
+
+        //Score
+        createHUD("Score", .15f, 0, score, 55, -15, 10, 10);
+
+        //Score Digits
+        for (int i = 1; i < 7; i++) {
+            createHUD("Score " + i + " Digit", 0, 0, numberArray[(int)Mathf.Floor(scoreData / Mathf.Pow(10, 6 - i)) % 10], 98 + (i - 1) * 9, -15, 7, 10);
+        }
+
+        //Timer
+        createHUD("Timer", .15f, 0, timer, 55, -27, 10, 10);
+
+        //Timer Digits
+        for (int i = 1; i < 4; i++) {
+            createHUD("Timer " + i + " Digit", 0, 0, numberArray[(int)Mathf.Floor(timerData / Mathf.Pow(10, 3 - i)) % 10], 98 + (i - 1) * 9, -27, 7, 10);
+        }
+
+        //Lives
+        createHUD("Lives", .02f, 0, lives, 32, -39, 10, 10);
+
+        //Lives Digits
+        for (int i = 1; i < 3; i++) {
+            createHUD("Lives " + i + " Digit", 0, 0, numberArray[(int)Mathf.Floor(livesData / Mathf.Pow(10, 2 - i)) % 10], 50 + (i - 1) * 9, -39, 7, 10);
+        }
+
+        //Spiritual Power
+        createHUD("Spirit Power", .025f, 0, spiritPower, 79, -39, 10, 10);
+
+        //Spirit Power Digits
+        for (int i = 1; i < 4; i++) {
+            createHUD("Spirit Power " + i + " Digit", 0, 0, numberArray[(int)Mathf.Floor(spiritData / Mathf.Pow(10, 3 - i)) % 10], 99 + (i - 1) * 9, -39, 7, 10);
+        }
+
+        //Stage
+        createHUD("Stage", .15f, 0, stage, 205, -15, 10, 10);
+
+        //Power Up
+        createHUD("Power Up", .1f, 0.05f, powerUp, 145, -35, 10, 10);
+
+        //Ryu's Health
+        createHUD("Ninja", .15f, 0, ninja, 205, -27, 10, 10);
+
+        //Ryu's Health Bars
+        for (int i = healthData; i < 16; i++) {
+            createHUD("Health " + i, 0, 0, healthEmpty, 247 + i * 7, -27, 6, 10);
+        }
+        for (int i = 0; i < healthData; i++) {
+            createHUD("Health " + i, 0, 0, healthFull, 247 + i * 7, -27, 6, 10);
+        }
     }
 
     private void createHUD(string gameObjectName, float localScaleX, float localScaleY, Texture2D textureUsed, float pixelInsetX, float pixelInsetY, float pixelInsetW, float pixelInsetH) {
@@ -95,5 +104,6 @@ public class GameData : MonoBehaviour {
         createdObject.transform.localScale = new Vector3(localScaleX, localScaleY, 1);
         createdObject.guiTexture.texture = textureUsed;
         createdObject.guiTexture.pixelInset = new Rect(pixelInsetX, pixelInsetY, pixelInsetW, pixelInsetH);
+        createdObject.transform.parent = GameObject.Find("Game HUD").transform;
     }
 }
