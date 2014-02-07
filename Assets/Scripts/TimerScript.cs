0cd5fa94f8f9b527e3d758dbc4226ebda6fcb22a
Anthony Kause <akause@barracudanetworks.com>
2014-02-06 22:49:15 -0500

    Added timer

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerScript : MonoBehaviour {
+
+	public GameData gameData;
+
+	void Start () {
+		gameData = GameObject.Find("Game HUD").GetComponent<GameData>();
+		InvokeRepeating("checkTime", 1, 1);
+	}
+
+	private void checkTime() {
+		gameData.timerData -= 1;
+		if (gameData.timerData == 0) {
+			// TODO kill Ryu, freeze everything, and reload scene
+		}
+	}
+}
